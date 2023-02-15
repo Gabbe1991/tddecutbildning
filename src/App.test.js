5b0011e7d8f9236f0367c5084dcf8dc8e3071d06
Gabriel <gabe.jara@gmail.com>
2023-02-15 18:44:57 +0100

    Blå/röd Fas

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test("Användarobjekt med samma användarnamn ska vara lika", () => {
-  const user1 = new App("Alice", "password1");
-  const user2 = new App("Alice", "password2");
-  expect(user1.equals(user2)).toBe(true);
+  const App1 = new App("Mohammed", "password1");
+  const App2 = new App("Mohammed", "password2");
+  expect(App1.equals(App2)).toBe(true);
+  expect(App1.Appname).toBe("Mohammed");
+  expect(App1.password).toBe("password1");
 });
+
 
