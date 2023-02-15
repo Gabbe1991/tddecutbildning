325a9acf6aed73806ec0431e244c81715a65afb2
Gabriel <gabe.jara@gmail.com>
2023-02-15 18:53:50 +0100

    Röd fas

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+import AuthService from './AuthService';
+
+test('AuthService should handle authentication and have a login method', () => {
+  const authService = new AuthService();
+  expect(typeof authService).toBe('object');
+  expect(typeof authService.login).toBe('function');
+});
+
+
 test("Användarobjekt med samma användarnamn ska vara lika", () => {
   const App1 = new App("Mohammed", "password1");
   const App2 = new App("Mohammed", "password2");
